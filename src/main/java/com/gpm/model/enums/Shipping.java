f3886234fe2d4a61121fb81dd3b69612b39d1a95
Mat Booth <mbooth@apache.org>
2013-05-12 20:28:56 +0100

    Add enum for shipping categories.

---
+++ src/main/java/com/gpm/model/enums/Shipping.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2013 Mat Booth <mbooth@apache.org>
+ */
+package com.gpm.model.enums;
+
+/**
+ * Enumeration for Royal Mail shipping categories.
+ * 
+ * @author mbooth
+ */
+public enum Shipping {
+  FIRST_UK, AIR_EUROPE, AIR_WORLD_1, AIR_WORLD_2
+}
